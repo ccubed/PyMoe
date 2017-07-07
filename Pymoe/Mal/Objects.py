912a6ec1830d86d044cf201e9763337a1725d143
Vectri <ctrlvectri@gmail.com>
2017-07-07 16:14:39 -0700

    Remove double format_data function and inconsistency with to_xml variables.

--- Pymoe/Mal/Objects.py
+++ Pymoe/Mal/Objects.py
@@ -98,10 +98,10 @@ class Anime:
                     elif x == 'dates':
                         if self.dates.user.start:
                             start = ET.SubElement(root, 'date_start')
-                            start.text = self.format_date(self.dates.user.start)
+                            start.text = str(self.format_date(self.dates.user.start))
                         if self.dates.user.end:
                             end = ET.SubElement(root, 'date_finish')
-                            end.text = self.format_date(self.dates.user.end)
+                            end.text = str(self.format_date(self.dates.user.end))
                     elif x == 'storage':
                         if self.storage.type:
                             stype = ET.SubElement(root, 'storage_type')
@@ -201,11 +201,6 @@ class Manga:
         self.xml_tags = ['id', 'chapters', 'volumes', 'scores', 'status', 'dates', 'storage', 'reread', 'flags',
                          'priority', 'comments', 'tags', 'scan_group']
 
-    @staticmethod
-    def format_date(data):
-        date_list = data.split("-")
-        return "{}{}{}".format(date_list[1], date_list[2], date_list[0])
-
     def to_xml(self):
         root = ET.Element("entry")
         for x in self.xml_tags:
@@ -230,10 +225,10 @@ class Manga:
                     elif x == 'dates':
                         if self.dates.user.start:
                             start = ET.SubElement(root, 'date_start')
-                            start.text = str(format_date(self.dates.user.start))
+                            start.text = str(self.format_date(self.dates.user.start))
                         if self.dates.user.end:
                             end = ET.SubElement(root, 'date_finish')
-                            end.text = str(format_date(self.dates.user.end))
+                            end.text = str(self.format_date(self.dates.user.end))
                     elif x == 'storage':
                         if self.storage.type:
                             stype = ET.SubElement(root, 'storage_type')
