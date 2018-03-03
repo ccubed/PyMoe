6675cf69905a5ec6faaee3b56f478bcd92b01f63
Lilac <ileyd@icloud.com>
2018-03-03 17:27:18 +1000

    add very basic mappings support

---
+++ Pymoe/Kitsu/mappings.py
@@ -0,0 +1,31 @@
+import requests
+from ..errors import *
+from .helpers import SearchWrapper
+
+
+class KitsuMappings:
+    def __init__(self, api, header):
+        self.apiurl = api
+        self.header = header
+
+    def get(self, external_site: str, external_id: int):
+        """
+        Get a kitsu mapping by external site ID
+
+        :param str external_site: string representing the external site
+        :param int external_id: ID of the entry in the external site.
+        :return: Dictionary or None (for not found)
+        :rtype: Dictionary or None
+        :raises: :class:`Pymoe.errors.ServerError`
+        """
+        r = requests.get(self.apiurl + "/mappings", params={"filter[externalSite]": external_site, "filter[externalId]": external_id}, headers=self.header)
+
+        if r.status_code != 200:
+          raise ServerError
+
+        jsd = r.json()
+
+        if len(jsd['data']) < 1:
+          return None
+
+        return jsd['data'][0]
